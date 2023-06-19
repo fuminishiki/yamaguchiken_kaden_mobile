eb050339643f12f5866f66b9c3c7d55a0b2eb4ec
fuminishiki <136785393+fuminishiki@users.noreply.github.com>
2023-06-19 12:28:56 +0900

    Create yamaguchiken_kaden_mobile.py

---
+++ yamaguchiken_kaden_mobile.py
@@ -0,0 +1,102 @@
+import folium
+
+m = folium.Map(location=[34.2629193,131.8501420],zoom_start=9)
+m
+
+folium.Marker(location=[34.1710095,132.2180211],popup='エディオン 岩国店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.0174643,131.8675565],popup='エディオン 下松店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.0700254,131.7830509],popup='エディオン 周南本店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.0071674,130.9457719],popup='エディオン 新下関店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[33.949076,130.9237044],popup='エディオン シーモール下関店　　　　　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.0550374,131.5586673],popup='エディオン 防府店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[33.9610585,132.1040929],popup='エディオン 柳井店　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[34.163606,131.4714769],popup='エディオン 山口本店　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[33.9838589,131.2053236],popup='エディオン ゆめタウン宇部店　　　　　　　　　　　　　',icon=folium.Icon(color="blue")).add_to(m)
+m
+
+folium.Marker(location=[33.9754777,130.919144],popup='家電住まいる館YAMADA下関店　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.069329,131.7603143],popup='家電住まいる館&YAMADA web.com新南陽店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0021497,131.3489655],popup='ベスト電器宇部東店　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[33.9626524,131.2386424],popup='ベスト電器山口宇部店　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.1605008,131.4701997],popup='ベスト電器山口店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0066833,130.9371179],popup='ベスト電器ゆめシティ下関本店　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0103895,131.3529191],popup='ヤマダデンキ テックランド阿知須店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.1737308,132.2253811],popup='ヤマダデンキ テックランド岩国店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[33.9767882,131.1872306],popup='ヤマダデンキ テックランド宇部店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0242238,131.0069949],popup='ヤマダデンキ テックランド下関長府店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0208731,131.8622335],popup='ヤマダデンキ テックランド周南店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4170197,131.4129755],popup='ヤマダデンキ テックランド萩店　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0496828,131.5400526],popup='ヤマダデンキ テックランド防府店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[33.9612374,132.1222306],popup='ヤマダデンキ テックランド柳井店　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.1388025,131.4420911],popup='ヤマダデンキ テックランド山口本店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3757222,131.1943224],popup='ヤマダデンキ テックランドNew長門店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[33.9351959,131.254959],popup='ヤマダデンキ ヤマダアウトレット宇部東店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.0920629,132.0831675],popup='ヤマダデンキ ヤマダアウトレット玖珂店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[33.9319064,131.2565312],popup='コジマ×ビックカメラ 山口宇部空港店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[33.9762346,131.2059636],popup='ケーズデンキ 宇部店　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[33.9547443,131.2580511],popup='パソコン工房 宇部店　　　　　　　　　',icon=folium.Icon(color="green")).add_to(m)
+m
+
+folium.Marker(location=[34.1561073,131.5010021],popup='パソコン工房 山口店　　　　　　　　　',icon=folium.Icon(color="green")).add_to(m)
+m
+
+folium.Marker(location=[34.3975903,132.4735216],popup='マーキュリー 中四国支店　　　　　　　　　　　',icon=folium.Icon(color="white")).add_to(m)
+m
+
+m.save('yamaguchiken_kaden_mobile.html')
